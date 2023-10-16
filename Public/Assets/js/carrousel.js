752ed8af24b8e306c3e6e63153cd4fcafb253aec
glouglouu <buiche.mathieu@gmail.com>
2023-10-16 22:06:04 +0200

    VDVZVZ

--- Public/Assets/js/carrousel.js
+++ Public/Assets/js/carrousel.js
@@ -1,7 +1,10 @@
 const booksContainer = document.querySelector(".books-container");
+
 let currentIndex = 0;
 const booksPerPage = 5; // Nombre de livres à afficher à la fois
 const maxBookLenght = 15;
+
+
 function displayBooks(startIndex) {
   booksContainer.innerHTML = "";
 
@@ -49,6 +52,7 @@ loadBooks()
   .then((data) => {
     books = data;
     displayBooks(currentIndex);
+    displayTopSellers();
   })
   .catch((error) => {
     console.error(error);
@@ -68,4 +72,44 @@ function nextBooks() {
     currentIndex = maxBookLenght - booksPerPage;
   }
   displayBooks(currentIndex);
+}
+
+
+// Fonction pour afficher un podium des 3 livres les plus vendus
+function displayTopSellers() {
+    // Trier les livres par nombre de ventes (à implémenter lorsque vous ajouterez le paramètre "nombre de ventes" à votre fichier book.json)
+    books.sort((a, b) => b.sales - a.sales);
+
+    // Sélectionner l'élément où afficher le podium (ajustez le sélecteur selon votre structure HTML)
+    const podiumContainer = document.querySelector('.podium-container');
+
+    // Effacer le contenu précédent du podium
+    podiumContainer.innerHTML = '';
+
+    // Créez un conteneur pour les livres du podium
+    const podiumBooksContainer = document.createElement('div');
+    podiumBooksContainer.classList.add('podium-books-container');
+
+    // Sélectionnez les 3 premiers livres (les plus vendus)
+    const topSellers = books.slice(0, 3);
+
+    // Affichez chaque livre du podium dans le conteneur
+    topSellers.forEach((book, index) => {
+        const bookHTML = `
+        <div class="wrappPodium">
+            <div class="podium-book">
+                <p>#${index + 1}</p> <!-- Ajoutez le numéro du podium -->
+                <a class="podium-book" href="productdetail.html?bookId=${book.id}">
+                <img src="${book.imageSrc}" alt="${book.title}">
+                <h2>${book.title}</h2>
+                </a>
+                <p class="sellNumber">Nombre de ventes : ${book.sales}</p>
+            </div>
+        </div>
+        `;
+        podiumBooksContainer.insertAdjacentHTML('beforeend', bookHTML);
+    });
+
+    // Ajoutez le conteneur du podium au conteneur principal
+    podiumContainer.appendChild(podiumBooksContainer);
 }
