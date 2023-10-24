2dd0b84caf5ab9be5ac7df205ad2868a1243651f
glouglouu <buiche.mathieu@gmail.com>
2023-10-24 11:22:00 +0200

    dzdzdzdzdz

--- Public/Assets/js/loader.js
+++ Public/Assets/js/loader.js
@@ -1,4 +1,8 @@
 // Fonction pour masquer la page "home"
+function hasVisitedBefore() {
+  return document.cookie.indexOf("visited=true") >= 0;
+}
+
 function hideHome() {
   const home = document.getElementById('home');
   home.style.display = 'none';
@@ -10,11 +14,27 @@ function showLoader() {
   loader.style.display = 'block';
 }
 
+// Fonction pour masquer le loader
+function hideLoader() {
+  const loader = document.querySelector('.loader');
+  loader.style.display = 'none';
+}
+
 // Masquez la page "home" au chargement du script
 hideHome();
 
-// Montrez le loader
-showLoader();
+
+if (hasVisitedBefore()) {
+  // Si l'utilisateur a déjà visité la page, masquez le loader immédiatement
+  hideLoader();
+  // Réaffichez la page "home"
+  const home = document.getElementById('home');
+  home.style.display = 'block';
+} else {
+  // Si c'est la première visite, affichez le loader et définissez un cookie pour indiquer que l'utilisateur a déjà visité la page
+  showLoader();
+  document.cookie = "visited=true; expires=Thu, 01 Jan 2030 00:00:00 UTC; path=/";
+}
 
 // Attendez que la page soit complètement chargée
 window.addEventListener('load', function() {
